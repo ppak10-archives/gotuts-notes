b2dbd7a0bab26d43ee60c53d4d4b770040662fad
Peter <ppak10@gmail.com>
2018-12-31 20:50:12 -0500

    web dev basics

--- gotut.go
+++ gotut.go
@@ -1,47 +1,18 @@
 package main
 
-import "fmt"
+import ("fmt"
+"net/http")
 
-const usixteenbitmax float64 = 65535
-const kmh_multiple float64 = 1.60934
+func index_handler(w http.ResponseWriter, r *http.Request) {
 
-type car struct {
-  gas_pedal uint16 // min0 max 65535
-  brake_pedal uint16
-  streering_wheel int16 // -32k - +32k
-  top_speed_kmh float64
+  fmt.Fprintf(w, `<h1>Hey there</h1>
+    <p>Go is fast!</p>
+    <p>and simple</p>
+    `)
+
 }
 
-func (c *car) kmh() float64 {
-  return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax)
-}
-
-func (c car) mph() float64 {
-  c.top_speed_kmh = 500
-  return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax/kmh_multiple)
-}
-
-func (c *car) new_top_speed(newspeed float64){
-  c.top_speed_kmh = newspeed
-}
-
-func newer_top_speed(c car, speed float64) car {
-  c.top_speed_kmh = speed
-  return c
-}
-
-func main() {
-  a_car := car{
-    gas_pedal: 65000,
-    brake_pedal: 0,
-    streering_wheel: 12561,
-    top_speed_kmh: 225.0}
-
-  fmt.Println(a_car.gas_pedal)
-  fmt.Println(a_car.kmh())
-  fmt.Println(a_car.mph())
-  //a_car.new_top_speed(500)
-  a_car = newer_top_speed(a_car,500)
-  fmt.Println(a_car.kmh())
-  fmt.Println(a_car.mph())
+func main () {
+  http.HandleFunc("/", index_handler)
+  http.ListenAndServe(":8000", nil)
 }
