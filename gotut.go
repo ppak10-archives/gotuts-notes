700fbb55a04e82737d9c537b54db12171390c04c
Peter <ppak10@gmail.com>
2018-12-31 20:04:16 -0500

    video 3

--- gotut.go
+++ gotut.go
@@ -1,8 +1,24 @@
 package main
 
-import ("fmt"
-"math/rand")
+import ("fmt")
+
+func add(x,y float32) float32 {
+  return x+y
+}
+
+func multiple(a,b string) (string,string) {
+  return a,b
+}
 
 func main() {
-  fmt.Println("A number from 1-100", rand.Intn(100))
+  /*num1,num2 := 5.6, 9.5
+  multiple lines*/
+
+  w1, w2 := "Hey","there"
+  fmt.Println(multiple(w1,w2))
+
+  // var a int = 62
+  // var b float64 = float64(a)
+  //
+  // x := a // x will be type int
 }
