91e18c69135439b6bc0c4ab038e939f763201f5c
Peter <ppak10@gmail.com>
2018-12-31 20:31:54 -0500

    value recievers

--- gotut.go
+++ gotut.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
 type car struct {
   gas_pedal uint16 // min0 max 65535
   brake_pedal uint16
@@ -9,12 +12,22 @@ type car struct {
   top_speed_kmh float64
 }
 
+func (c car) kmh() float64 {
+  return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax)
+}
+
+func (c car) mph() float64 {
+  return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax/kmh_multiple)
+}
+
 func main() {
   a_car := car{
-    gas_pedal: 22341,
+    gas_pedal: 65000,
     brake_pedal: 0,
     streering_wheel: 12561,
     top_speed_kmh: 225.0}
 
   fmt.Println(a_car.gas_pedal)
+  fmt.Println(a_car.kmh())
+  fmt.Println(a_car.mph())
 }
