a59d146c901cc2108c8f90a38446e81e58d91268
Peter <ppak10@gmail.com>
2018-12-31 20:42:31 -0500

    methods

--- gotut.go
+++ gotut.go
@@ -12,12 +12,22 @@ type car struct {
   top_speed_kmh float64
 }
 
-func (c car) kmh() float64 {
+func (c *car) kmh() float64 {
   return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax)
 }
 
 func (c car) mph() float64 {
+  c.top_speed_kmh = 500
   return float64(c.gas_pedal) * (c.top_speed_kmh/usixteenbitmax/kmh_multiple)
+}
+
+func (c *car) new_top_speed(newspeed float64){
+  c.top_speed_kmh = newspeed
+}
+
+func newer_top_speed(c car, speed float64) car {
+  c.top_speed_kmh = speed
+  return c
 }
 
 func main() {
@@ -28,6 +38,10 @@ func main() {
     top_speed_kmh: 225.0}
 
   fmt.Println(a_car.gas_pedal)
+  fmt.Println(a_car.kmh())
+  fmt.Println(a_car.mph())
+  //a_car.new_top_speed(500)
+  a_car = newer_top_speed(a_car,500)
   fmt.Println(a_car.kmh())
   fmt.Println(a_car.mph())
 }
