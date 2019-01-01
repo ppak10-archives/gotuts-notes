d2ca6dfbec56beefe231c27c25572d42349a70d1
Peter <ppak10@gmail.com>
2018-12-31 21:33:10 -0500

    video 13

--- gotut.go
+++ gotut.go
@@ -1,7 +1,6 @@
 package main
 
-import ("fmt"
-"net/http"
+import ("net/http"
 "io/ioutil"
 "encoding/xml")
 
@@ -65,25 +64,25 @@ https://www.washingtonpost.com/news-sitemaps/goingoutguide.xml
 </sitemapindex>
  */
 type SitemapIndex struct {
-  Locations []Location `xml:"sitemap"`
+  Locations []string `xml:"sitemap>loc"`
 }
 
-type Location struct {
-  Loc string `xml:"loc"`
+type News struct {
+  Titles []string `xml:"url>news>title"`
+  Keywords []string `xml:"url>news>Keywords"`
+  Locations []string `xml:"url>loc"`
 }
 
-func (l Location) String() string {
-  return fmt.Sprintf(l.Loc)
-}
 func main () {
+  var s SitemapIndex
+  var n News
   resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
   bytes, _ := ioutil.ReadAll(resp.Body)
-  resp.Body.Close()
-
-  var s SitemapIndex
   xml.Unmarshal(bytes, &s)
 
   for _, Location := range s.Locations {
-    fmt.Printf("\n%s", Location)
+    resp, _ := http.Get(Location)
+    bytes, _ := ioutil.ReadAll(resp.Body)
+    xml.Unmarshal(bytes, &n)
   }
 }
