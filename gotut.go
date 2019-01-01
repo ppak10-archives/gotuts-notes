9e2fa38773d58c0e180eb3ea1729c37255228758
Peter <ppak10@gmail.com>
2018-12-31 20:08:52 -0500

    video 4
    Pointers

--- gotut.go
+++ gotut.go
@@ -2,23 +2,15 @@ package main
 
 import ("fmt")
 
-func add(x,y float32) float32 {
-  return x+y
-}
-
-func multiple(a,b string) (string,string) {
-  return a,b
-}
-
 func main() {
-  /*num1,num2 := 5.6, 9.5
-  multiple lines*/
+  x := 15
+  a := &x // memory address
+  fmt.Println(a)
+  fmt.Println(*a)
+  *a = 5
+  fmt.Println(x)
+  *a = *a**a
+  fmt.Println(x)
+  fmt.Println(*a)
 
-  w1, w2 := "Hey","there"
-  fmt.Println(multiple(w1,w2))
-
-  // var a int = 62
-  // var b float64 = float64(a)
-  //
-  // x := a // x will be type int
 }
