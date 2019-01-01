a242bbf9ac839ec847a0b054f4ea073972f6ff43
Peter <ppak10@gmail.com>
2018-12-31 21:39:24 -0500

    Maps

--- gotut.go
+++ gotut.go
@@ -1,88 +1,21 @@
 package main
 
-import ("net/http"
-"io/ioutil"
-"encoding/xml")
+import "fmt"
 
-/*
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<sitemap>
-<loc>
-https://www.washingtonpost.com/news-sitemaps/politics.xml
-</loc>
-</sitemap>
-<sitemap>
-<loc>
-https://www.washingtonpost.com/news-sitemaps/opinions.xml
-</loc>
-</sitemap>
-<sitemap>
-<loc>
-https://www.washingtonpost.com/news-sitemaps/local.xml
-</loc>
-</sitemap>
-<sitemap>
-<loc>
-https://www.washingtonpost.com/news-sitemaps/sports.xml
-</loc>
-</sitemap>
-<sitemap>
-<loc>
-https://www.washingtonpost.com/news-sitemaps/national.xml
-</loc>
-</sitemap>
-<sitemap>
-<loc>
-https://www.washingtonpost.com/news-sitemaps/world.xml
-</loc>
-</sitemap>
-<sitemap>
-<loc>
-https://www.washingtonpost.com/news-sitemaps/business.xml
-</loc>
-</sitemap>
-<sitemap>
-<loc>
-https://www.washingtonpost.com/news-sitemaps/technology.xml
-</loc>
-</sitemap>
-<sitemap>
-<loc>
-https://www.washingtonpost.com/news-sitemaps/lifestyle.xml
-</loc>
-</sitemap>
-<sitemap>
-<loc>
-https://www.washingtonpost.com/news-sitemaps/entertainment.xml
-</loc>
-</sitemap>
-<sitemap>
-<loc>
-https://www.washingtonpost.com/news-sitemaps/goingoutguide.xml
-</loc>
-</sitemap>
-</sitemapindex>
- */
-type SitemapIndex struct {
-  Locations []string `xml:"sitemap>loc"`
-}
+func main() {
+  grades := make(map[string]float32)
 
-type News struct {
-  Titles []string `xml:"url>news>title"`
-  Keywords []string `xml:"url>news>Keywords"`
-  Locations []string `xml:"url>loc"`
-}
+  grades["Timmy"] = 42
+  grades["Jess"] = 92
+  grades["Sam"] = 67
 
-func main () {
-  var s SitemapIndex
-  var n News
-  resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
-  bytes, _ := ioutil.ReadAll(resp.Body)
-  xml.Unmarshal(bytes, &s)
+  TimsGrade := grades["Timmy"]
+  fmt.Println(TimsGrade)
 
-  for _, Location := range s.Locations {
-    resp, _ := http.Get(Location)
-    bytes, _ := ioutil.ReadAll(resp.Body)
-    xml.Unmarshal(bytes, &n)
+  delete(grades, "Timmy")
+  fmt.Println(grades)
+
+  for k, v := range grades {
+    fmt.Println(k,":",v)
   }
 }
